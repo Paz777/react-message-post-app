728fa3cff1e9b09d5e2f5e5839c0a8b1b714b6ad
Sonagara <Paz.Sonagara@roke.co.uk>
2023-07-28 14:52:19 +0100

    Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,7 @@
+const name = ['Hooby', 'Dobby'];
+
 function Post() {
+    const chosenName = Math.random() > 0.5 ? name[0] : name[1];
     return <div>
         <p>Hooby Dooby</p>
         <p>Learning react</p>
