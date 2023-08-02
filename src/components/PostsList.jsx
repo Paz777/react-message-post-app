86f5ec98d01dfecfcad5a7f6233be2ac2a55a96c
Paz777 <paz.sonagara@gmail.com>
2023-08-02 16:45:32 +0100

    Adding in new post component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,15 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css'
 
 function PostsList() {
-    return <ul className={classes.posts}>
-        <Post author="Hobby" body="React is reactive" />
-        <Post author="Dobby" body="I am learning react" />
-    </ul>
+    return <>
+        <NewPost />
+        <ul className={classes.posts}>
+            <Post author="Hobby" body="React is reactive" />
+            <Post author="Dobby" body="I am learning react" />
+        </ul>
+    </>
 }
 
 export default PostsList;
