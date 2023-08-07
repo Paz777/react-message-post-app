eaf4f868678277a815adcecf98521e4304862d45
Paz777 <111736251+Paz777@users.noreply.github.com>
2023-08-07 09:30:07 +0100

    Added modal overlay - used as a wrapper for other components

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,6 +1,7 @@
 import NewPost from './NewPost';
 import Post from './Post';
-import classes from './PostsList.module.css'
+import Modal from './Modal';
+import classes from './PostsList.module.css';
 import { useState } from 'react';
 
 function PostsList() {
@@ -13,11 +14,13 @@ function PostsList() {
     }
 
     function onAuthorHandler(event) {
-        setEnteredAuthor(event.target.value)
+        setEnteredAuthor(event.target.value);
     }
 
     return <>
-        <NewPost onBodyChange={onBodyHandler} onAuthorChange={onAuthorHandler} />
+        <Modal>
+            <NewPost onBodyChange={onBodyHandler} onAuthorChange={onAuthorHandler} />
+        </Modal>
         <ul className={classes.posts}>
             <Post author={enteredAuthor} body={enteredBody} />
             <Post author="Dobby" body="I am learning react" />
